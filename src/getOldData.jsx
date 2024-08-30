2dda8e01ffe6cd7e4002a138f6737e73658ae901
araghyakaleeb <araghyakaleeb@gmail.com>
2024-08-30 07:42:26 +0000

    rebuilding everything

--- src/getOldData.jsx
+++
@@ -1,9 +0,0 @@
-
-function GetOldData() {
-    document.getElementById('entryDisplay').innerHTML = localStorage.getItem('entries');
-
-    const deleteButtons = document.querySelectorAll('.delButton');
-    deleteButtons.forEach(button => button.remove());
-}
-
-export default GetOldData
